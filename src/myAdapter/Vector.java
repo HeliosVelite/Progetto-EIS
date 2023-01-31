155f0ff1e67f95fe98f075f0c248951da266eb5b
HeliosVelite <heliosvelite@gmail.com>
2023-01-31 16:24:28 +0100

    aaaa

--- src/myAdapter/Vector.java
+++ src/myAdapter/Vector.java
@@ -1,6 +1,7 @@
 package myAdapter;
 
 import java.util.Enumeration;
+import java.util.NoSuchElementException;
 
 /**
  * Class Vector of Java Micro Edition CLDC1.1
@@ -11,7 +12,7 @@ import java.util.Enumeration;
  * Each vector tries to optimize storage management by maintaining a capacity and a capacityIncrement. The capacity is always at least as large as the vector size; it is usually larger because as components are added to the vector, the vector's storage increases in chunks the size of capacityIncrement. An application can increase the capacity of a vector before inserting a large number of components; this reduces the amount of incremental reallocation.
  * </p>
  * <b>This class purpose is to force Java Micro Edition CLDC1.1 version of the Vector class into a work environment with the last version of Java</b>
- * @see <a href="https://docs.oracle.com/javame/config/cldc/ref-impl/cldc1.1/jsr139/index.html">CLDC1.1 Vector</a>
+ * @see <a href="https://docs.oracle.com/javame/config/cldc/ref-impl/cldc1.1/jsr139/index.html">CLDC1.1</a>
 
  * @author Zuech Riccardo
  */
@@ -47,7 +48,7 @@ public class Vector {
         vector.copyInto(anArray);
     }
 
-    public Object elementAt(int index){
+    public Object elementAt(int index) throws ArrayIndexOutOfBoundsException{
         return vector.elementAt(index);
     }
 
@@ -59,7 +60,7 @@ public class Vector {
         vector.ensureCapacity(minCapacity);
     }
 
-    public Object firstElement(){
+    public Object firstElement() throws NoSuchElementException{
         return vector.firstElement();
     }
 
@@ -79,7 +80,7 @@ public class Vector {
         return vector.isEmpty();
     }
 
-    public Object lastElement(){
+    public Object lastElement() throws NoSuchElementException{
         return vector.lastElement();
     }
 
@@ -99,11 +100,11 @@ public class Vector {
         return vector.removeElement(obj);
     }
 
-    public void removeElementAt(int index){
+    public void removeElementAt(int index) throws ArrayIndexOutOfBoundsException{
         vector.removeElementAt(index);
     }
 
-    public void setElementAt(Object obj, int index){
+    public void setElementAt(Object obj, int index) throws ArrayIndexOutOfBoundsException{
         vector.setElementAt(obj, index);
     }
 
